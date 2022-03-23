4c92d355f91630a8772673758b4870d85f7c339d
Andreas Anckar <andreas.anckar@gmail.com>
2022-03-23 21:44:43 +0200

    Add row actions

--- js/product-table.js
+++ js/product-table.js
@@ -1,12 +1,12 @@
 import { delay } from "./utils.js";
 
-async function fetchProductData(sku) {
+async function fetchProductData(sku, rowId) {
   const params = new URLSearchParams();
   params.append("sku", sku);
   await delay(2000);
   const res = await fetch(`/api/product-data.php?${params.toString()}`);
   const json = await res.json();
-  const tr = document.querySelector(`tr[data-id="${sku}"]`);
+  const tr = document.querySelector(`tr[data-id="${rowId}"]`);
   if (tr) {
     tr.querySelector("td[data-col='name']").innerHTML = json.name;
     tr.querySelector("td[data-col='price']").innerHTML = json.price;
@@ -14,7 +14,7 @@ async function fetchProductData(sku) {
   }
 }
 
-function createTd(data) {
+function createCol(data) {
   const { name, content } = data;
   const td = document.createElement("td");
   td.dataset.col = name;
@@ -23,28 +23,37 @@ function createTd(data) {
 }
 
 function createRow(data) {
-  const { sku } = data;
+  const { id, sku } = data;
   const tr = document.createElement("tr");
-  tr.dataset.id = sku;
+  tr.dataset.id = id;
   const cols = [
     { name: "sku", content: sku },
     { name: "name" },
     { name: "price" },
     { name: "detailsUrl" },
   ];
-  const tds = cols.map((col) => createTd(col));
+  const tds = cols.map((col) => createCol(col));
   tds.forEach((td) => tr.appendChild(td));
+  const actionsTd = document.createElement("td");
+  const removeBtn = document.createElement("button");
+  removeBtn.innerHTML = "Del";
+  removeBtn.type = "button";
+  removeBtn.addEventListener("click", (e) => {
+    tr.remove();
+  });
+  actionsTd.appendChild(removeBtn);
+  tr.appendChild(actionsTd);
   return tr;
 }
 
 export default function initProductTable() {
   const form = document.querySelector("[data-add-products-form]");
-  const input = form.querySelector("input[type='text']");
   const table = document.querySelector("[data-add-products-table]");
 
   if (!form || !table) {
     return;
   }
+  const input = form.querySelector("input[type='text']");
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -52,12 +61,14 @@ export default function initProductTable() {
     const sku = formData.get("product-sku");
     form.reset();
     input.focus();
-    addProduct(sku);
-    fetchProductData(sku);
+    const rowId = addProduct(sku);
+    fetchProductData(sku, rowId);
   });
 
   function addProduct(sku) {
-    const tr = createRow({ sku });
+    const id = Date.now();
+    const tr = createRow({ id, sku });
     table.appendChild(tr);
+    return id;
   }
 }
